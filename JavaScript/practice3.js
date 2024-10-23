59712d4c9bdc77477f7a6e0f8d4f6339cf96bdd8
Hidetoshi tsutsumi <tsutsutsutsumi223@gmail.com>
2024-10-23 13:54:40 +0000

    Javascriptの基本文法を練習

--- JavaScript/practice3.js
+++ JavaScript/practice3.js
@@ -7,3 +7,19 @@ function maxAndAverage(array){
 
 let test_array = [1, 2, 3, 4, 5];
 alert(JSON.stringify(maxAndAverage(test_array)));
+
+function checkPalindromes(words) {
+  let result_object = {};
+  for(let word of words){
+    let reversed = word.split('').reverse().join('');
+    if(word == reversed){
+      result_object[word] = true;
+    } else{
+      result_object[word] = false;
+    }
+  }
+  return result_object;
+}
+
+let test_words = ['madam', 'apple', 'racecar'];
+alert(JSON.stringify(checkPalindromes(test_words)));
