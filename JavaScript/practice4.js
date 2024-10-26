ff87158b3dd3acb8c162060703e8cbb2d0bbb926
Hidetoshi tsutsumi <tsutsutsutsumi223@gmail.com>
2024-10-26 14:45:52 +0000

    Javascriptの基本文法を練習

--- JavaScript/practice4.js
+++ JavaScript/practice4.js
@@ -9,3 +9,17 @@ function removeDuplicates(integer_array){
 }
 alert(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
 
+function rotateLeft(array, k) {
+  let return_array = [];
+  let length = array.length;
+  k = k % length; // kが長さを超えないように
+
+  for (let i = 0; i < length; i++) {
+    return_array.push(array[(i + k) % length]); // 回転したインデックスを計算
+  }
+  
+  return return_array;
+}
+
+let array_for_left = [1, 2, 3, 4, 5, 6, 7, 8 ,9];
+alert(rotateLeft(array_for_left, 2));
